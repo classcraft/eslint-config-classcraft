535c6eb125858bd8018f5f99714e66a91690bf78
Alex Nault <nault.alex@gmail.com>
2017-07-03 12:14:37 -0400

    Add initial config

---
+++ index.js
@@ -0,0 +1,21 @@
+module.exports = {
+    extends: "eslint-config-airbnb",
+    rules: {
+        indent: [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        quotes: [
+            "error",
+            "double"
+        ],
+        semi: [
+            "error",
+            "always"
+        ]
+    }
+};
