9ed3e35866c54635cfe0ecc689726d49e39420aa
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2018-08-01 16:37:53 -0400

    Add prettier ignore (#8)

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 module.exports = {
-    extends: ["eslint-config-airbnb", "plugin:prettier/recommended"],
+    extends: [
+        "eslint-config-airbnb",
+        "plugin:prettier/recommended",
+        "prettier/flowtype",
+        "prettier/react"
+    ],
     rules: {
         "import/no-absolute-path": 0,
         "react/prefer-stateless-function": 0
