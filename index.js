f17d77afd5505f1ed2c66405b99a9c3ee87cbdc6
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2018-09-26 10:41:54 -0400

    Add flowtype plugin (#9)

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ module.exports = {
     extends: [
         "eslint-config-airbnb",
         "plugin:prettier/recommended",
+        "plugin:flowtype/recommended",
         "prettier/flowtype",
         "prettier/react"
     ],
+    parser: "babel-eslint",
+    plugins: ["flowtype"],
     rules: {
         "import/no-absolute-path": 0,
-        "react/prefer-stateless-function": 0
+        "react/prefer-stateless-function": 0,
+        "flowtype/delimiter-dangle": ["error", "always-multiline"],
+        "flowtype/semi": ["error", "always"],
+        "flowtype/no-primitive-constructor-types": 2
     }
 };
